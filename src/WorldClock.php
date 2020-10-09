6baf859ad5dd2afb3e3ce784fe49bab23c656399
amidesfahani <61671721+amidesfahani@users.noreply.github.com>
2020-10-09 13:48:50 +0330

    Create WorldClock.php

---
+++ src/WorldClock.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AmidEsfahani\WorldClock;
+
+use Illuminate\Support\Carbon;
+use Laravel\Nova\Card;
+
+class WorldClock extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = '1/3';
+
+    /**
+     * Get the component name for the element.
+     *
+     * @return string
+     */
+    public function component()
+    {
+        return 'world-clock';
+    }
+
+    public function timezones($timezones) {
+
+        return $this->withMeta([
+            'timezones' => $timezones
+        ]);
+    }
+
+    public function timeFormat($timeFormat) {
+
+        return $this->withMeta([
+            'timeFormat' => $timeFormat
+        ]);
+    }
+
+    public function updatePeriod($ms = 1000) {
+        return $this->withMeta([
+            'ms' => $ms
+        ]);
+    }
+}
