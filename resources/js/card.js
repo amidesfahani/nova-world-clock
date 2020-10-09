a82c138b0327667e26b072dbeb7163e4651ef03e
amidesfahani <61671721+amidesfahani@users.noreply.github.com>
2020-10-09 15:41:25 +0330

    Update card.js

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,10 +1,10 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faSun,faMoon} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 library.add(faSun, faMoon)
 
-Nova.booting((Vue, router) => {
-    Vue.component('world-clock', require('./components/Card'));
+Nova.booting((Vue, router, store) => {
+    Vue.component('NovaWorldClock', require('./components/Card'));
     Vue.component('font-awesome-icon', FontAwesomeIcon)
 })
