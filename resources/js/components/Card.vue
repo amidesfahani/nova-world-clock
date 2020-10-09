b213b53e4e0db8c5d28f0d183e74993b1a43aa37
amidesfahani <61671721+amidesfahani@users.noreply.github.com>
2020-10-09 15:42:06 +0330

    Update Card.vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,16 +1,15 @@
 <template>
-    <card class="h-auto">
-        <div class="p-3" id="app">
-          <div class="flex flex-col">
-              <div class="flex flex-row content-center items-center" v-for="time in this.times">
-                <div class="p-2">{{time.name}}</div>
-                <div class="p-2">
-                  {{time.time}}
-                  <span v-show="time.night" style="color:#F7941D;"><font-awesome-icon icon="moon" /></span>
-                  <span v-show="!time.night" style="color:#F7941D;"><font-awesome-icon icon="sun" /></span>
+    <card class="flex flex-col items-center justify-center">
+        <div class="w-full px-3 py-3">
+            <h1 v-show="card.showtitle" class="text-center text-3xl text-80 font-light">{{ card.title }}</h1>
+            <div class="flex flex-row content-center items-center border-b border-gray-200" v-for="time in this.times" :key="time.name">
+                <div class="w-3/5 p-2">{{time.name}}</div>
+                <div class="w-2/5 p-2">{{time.time}}</div>
+                <div class="w-1/5">
+                    <span v-show="time.night" style="color:#F7941D;"><font-awesome-icon icon="moon" /></span>
+                    <span v-show="!time.night" style="color:#F7941D;"><font-awesome-icon icon="sun" /></span>
                 </div>
-              </div>
-          </div>
+            </div>
         </div>
     </card>
 </template>
@@ -32,7 +31,7 @@
     methods: {
       getTime() {
         this.timezones = [];
-        Nova.request().post('/nova-vendor/world-clock/get_times', {
+        Nova.request().post('/nova-vendor/nova-world-clock/get_times', {
           timezones: this.card.timezones,
           timeFormat: this.card.timeFormat
         })
