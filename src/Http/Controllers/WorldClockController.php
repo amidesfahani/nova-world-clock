752619404ebac8e8a210e7b905980d4720808618
amidesfahani <61671721+amidesfahani@users.noreply.github.com>
2020-10-09 13:50:40 +0330

    Create WorldClockController.php

---
+++ src/Http/Controllers/WorldClockController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AmidEsfahani\WorldClock\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class WorldClockController
+{
+    public function getTimes(Request $request)
+    {
+        if (!$request->timeFormat){
+            $request->timeFormat = 'h:i:s';
+        }
+        $times = [];
+        foreach ($request->timezones as $timezone){
+            $time = Carbon::now($timezone);
+            $night = (int) $time->format('H') > 17;
+            $name = explode('/',$time->getTimezone()->getName())[1];
+            $name = str_replace('_',' ',$name);
+            array_push($times,[
+                'name'=> ucwords($name),
+                'time'=> $time->format($request->timeFormat),
+                'night'=> $night
+            ]);
+        }
+        return $times;
+    }
+}
