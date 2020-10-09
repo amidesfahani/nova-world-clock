c1a6f5c438155b0d49f2c644ba88f9eb3791fa8d
amidesfahani <61671721+amidesfahani@users.noreply.github.com>
2020-10-09 15:57:57 +0330

    Update Card.vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
-    <card class="flex flex-col items-center justify-center">
+    <card class="flex flex-col items-center justify-center" :class="{'h-auto': card.showtitle}">
         <div class="w-full px-3 py-3">
-            <h1 v-show="card.showtitle" class="text-center text-3xl text-80 font-light">{{ card.title }}</h1>
-            <div class="flex flex-row content-center items-center border-b border-gray-200" v-for="time in this.times" :key="time.name">
+            <h1 v-show="card.showtitle" class="text-center text-2xl text-80 font-light m-3 p-2 border-b border-gray-300">{{ card.title }}</h1>
+            <div class="flex flex-row content-center items-center border-b border-gray-200 last:border-b-0" v-for="time in this.times" :key="time.name">
                 <div class="w-3/5 p-2">{{time.name}}</div>
                 <div class="w-2/5 p-2">{{time.time}}</div>
                 <div class="w-1/5">
